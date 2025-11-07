045101a60a1a0cc6b54dc5a96feee8a7fe3a4bd7
Matheus T. dos Santos <matheuswhite@citrinio.com>
2025-11-07 13:18:27 -0300

    feat: camera movement

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,7 +1,10 @@
 extends Camera3D
 
 @export var white_ball: RigidBody3D
-const speed = 1
+var azimuth: float = 0.0
+var elevation: float = PI / 4
+var sensitivity: float = 0.05
+var direction: Vector3 = Vector3(0, 0, -1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,15 +13,28 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position = white_ball.position + Vector3(0, 50, 30)
+	var distance = 30
+	
+	if Input.is_key_pressed(KEY_LEFT):
+		azimuth += sensitivity
+	if Input.is_key_pressed(KEY_RIGHT):
+		azimuth -= sensitivity
+	if Input.is_key_pressed(KEY_UP):
+		elevation += sensitivity
+	if Input.is_key_pressed(KEY_DOWN):
+		elevation -= sensitivity
+		
+	elevation = clampf(elevation, 0, PI / 3)
+
+	var x = distance * cos(elevation) * sin(azimuth)
+	var y = distance * sin(elevation)
+	var z = distance * cos(elevation) * cos(azimuth)
+	position = white_ball.position + Vector3(x, y, z)
+	direction = -Vector3(x, 0, z).normalized()
+	look_at(white_ball.position + Vector3(0, 10, 0))
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			global_position -= global_basis.z * speed
-		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			global_position += global_basis.z * speed
-	elif event is InputEventKey:
+	if event is InputEventKey:
 		if event.is_released() and event.keycode == KEY_SPACE:
-			white_ball.apply_impulse(Vector3(0, 0, -200), Vector3(0, 1, 0))
+			white_ball.apply_impulse(direction * 200, Vector3(0, 1, 0))
